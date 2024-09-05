00cfbec2af0cf8f5530bbc052111b4cbbc77d4f8
Kijima Daigo <norimaking777@gmail.com>
2024-09-05 20:49:10 +0900

    feat: 永続ログ取得エンドポイントを作成した

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from tasks import run
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -10,12 +11,28 @@ def root():
     }
     return jsonify(data)
 
+# タスク投入エンドポイント
 @app.route('/enqueue', methods=['POST'])
 def enqueue():
     json = request.get_json()
     cmd = json['cmd']
+
     result = run.delay(cmd)
     data = {
         "task_id": result.id,
     }
     return jsonify(data)
+
+# 永続ログ取得エンドポイント
+@app.route('/permlog', methods=['GET'])
+def permlog():
+    req = request.args
+    task_id = req.get("task_id")
+
+    # MEMO: ログファイル名の規則はfluentdで定義されている
+    filename = os.path.join("/log", f"worker_tagged.{task_id}.log")
+    f = open(filename, 'r')
+    log = f.read()
+    f.close()
+
+    return log
